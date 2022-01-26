af6c22e7f78e7c61a441644670c6ef2f085b8697
alekel133 <80880723+alekel133@users.noreply.github.com>
2022-01-26 11:42:20 -0600

    Add files via upload

---
+++ SysProg/Program_1/C++/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "List.h"
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    List list;
+    string input; 
+
+    cout << "Enter strings to be reversed. Enter [STOP] when finished:" << endl;
+
+    while(input != "STOP")
+    {
+        cin >> input;
+        list.insert(input); 
+    }
+
+    cout << endl << "Reversed: " << endl;
+
+    list.reverse(cout);
+
+    return 0;
+}

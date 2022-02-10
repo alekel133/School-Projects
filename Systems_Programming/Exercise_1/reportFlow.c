30f94258cd09d30abecac59512bd7ac4bc3c11c5
Alexander Kellough <atk133@msstate.edu>
2022-02-10 06:34:24 -0600

    Finished program 2 (in Exercise 1

--- Systems_Programming/Exercise_1/reportFlow.c
+++ Systems_Programming/Exercise_1/reportFlow.c
@@ -1,12 +1,20 @@
-
+/*      
+ *      Author: Alexander Kellough
+ *      MSU NetID: atk133
+ *      Compiler: GCC
+ *      Description:
+ *          Reads in data from a specified file (defailt flowdata.dat) and reports on the flow represented by the data on that file.
+ */
 #include "hdrs.h"
 
+// Union for converting 2bit values to 4 bit integers.
 union ISTR
 {
     char bytes[4];
     int val;  
 };
 
+// Checks data, and reports it's flow if necessary. Returns -1 or 0 based on if a report was made or not.
 int reportFlow(int val)
 {
     if(val < 550)
@@ -30,24 +38,32 @@ int main(int argc, char *argv[])
     int fd;
     union ISTR in;
 
-    if(argc != 1 && argc != 2)
+    if(argc != 1 && argc != 2) // Checks if command line arguments match syntax.
     {
         perror("Syntax reportFlow <filename (optional)>\n");
         exit(EXIT_FAILURE);
     }
     
-    if(argc == 2)
+    if(argc == 2) // If optional command line argument is given, assigns it to filename.
     {
         sscanf(argv[1], "%s", filename);
     }
 
     fd = open (filename, O_RDONLY); 
 
-    while(read(fd, in.bytes, 2))
+    if(fd == -1)
     {
-        reset = reportFlow(in.val);
-        ++i;
-        if(reset == -1)
+        perror("open(filename)");
+        exit(EXIT_FAILURE);
+    }
+
+    while(read(fd, in.bytes, 2)) // Reads in 2 bytes of data from input file.
+    {
+        reset = reportFlow(in.val); // Sends data to reportFlow(int val), for analysis and reporting.
+                                    
+        ++i; // Keeps count of data memebers read since last report.
+
+        if(reset == -1) // If a report was printed, prints readings since last report and resets count.
         {
             printf("%d readings since last report.\n", i);
             i = 0;

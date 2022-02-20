149aed14e43d5c34975c66e74390ff7234c94d6c
Alexander Kellough <atk133@msstate.edu>
2022-02-20 14:46:38 -0600

    Test_1 Partial completion

---
+++ Systems_Programming/Test_1/skel.c
@@ -0,0 +1,225 @@
+/* All of the includes for the project go here … 
+ * those includes must support signals, interval timers,
+ * read, open, and close. You will also need support for 
+ * error numbers
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Problem 1  */
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+
+#define BUF_SIZE 1024
+
+static int timed;
+
+/* the myPrint function takes a constant c-string
+ * and outputs it to STDOUT using write
+ * myPrint returns 0 on failure and 1 on success
+ */
+int myPrint(const char *str)
+{
+/* Problem 2 */ 
+    if(write(1, str, strlen(str)) == -1)
+    {
+        return 0;
+    }
+    return 1;
+}
+
+/* the myPrintInt function takes a constant int
+ * and outputs it to STDOUT using write
+ * myPrintInt returns 0 on failure and 1 on success
+ */
+int myPrintInt(const int val)
+{
+/* Problem 3 */ 
+    char *str;
+    int pass = val;
+    sscanf(str, "%d", &pass);
+    if(myPrint(str) == 0) 
+        return 0;
+    return 1;
+}
+
+/* implement the signal hadler below ...
+ * use the provided prototype
+ * It must handle two signals, SIGINT and SIGALRM
+ */
+void signalHandler(int sig)
+{
+/* Problem 4 */
+    if(sig == SIGINT)
+    {
+        char *reply;
+        write(1,"Do you wish to exit?{y/n}\n", 27);
+        read(STDIN_FILENO, reply, 2);
+        if(strcmp(reply, "y") == 0 || strcmp(reply, "Y") == 0)
+            exit(EXIT_SUCCESS);
+        else
+            return;
+    }
+    if(sig == SIGALRM)
+        timed = 1;
+    return;
+}
+/* This function reads a line of text from a file
+ * we are simulating another functions behavior here ..
+ * Return a 0 if no characters were read, return a 1 otherwise
+ */
+int readLine(int fd, char *line)
+{
+/* Problem 5 */
+char *buf;
+int i = 0;
+    while(*buf != '\n')
+    {
+       read(fd, buf, 1);        
+       if(*buf == '\0')
+           return 0;
+       line[i] = *buf;
+       ++i;
+    }
+    return i;
+}
+
+
+
+/* This function reads a question answer pairing
+ * from the provided pair of file descriptors
+ * It returns 0 if the files are empty
+ * and 1 if if successfully reads the pairing
+ */
+
+int readQA(int questFd, int ansFd, char *quest, char *ans)
+{
+  if (readLine(questFd,quest) == 0) return 0; 
+  if (readLine(ansFd, ans) == 0) return 0;
+  return 1;
+}
+
+int main(int argc, char *argv[])
+{
+
+  int numRead = 0;
+  int numWrite = 0;
+  int question = 1;
+  int correct = 0;
+  char buf[BUF_SIZE];
+  char quest[BUF_SIZE];
+  char ans[BUF_SIZE];
+  int questFd, ansFd;
+
+ /* Problem 6a*/
+struct sigaction sa;
+
+ /* Problem 6b */ 
+sigemptyset(&sa.sa_mask);
+sigaddset(&sa.sa_mask, SIGINT);
+sigaddset(&sa.sa_mask, SIGALRM);
+sa.sa_handler = signalHandler;
+sa.sa_flags = 0;
+
+/* Problem 6c */
+
+
+/* Problem 6d */
+
+
+/* Problem 7 */
+
+  
+
+/* Problem 8  */
+    
+   /* this loop handles the Q/A stuff 
+    * I have included some of it to make you life simpler 
+    * I have also left some commnents to assist you as well 
+    *
+    * read the first question, answer pairing prior to entering the loop
+    */
+    readQA(questFd, ansFd, quest, ans);
+  while (1)
+    {
+      /* output the current question */
+      myPrint("#");
+      myPrintInt(question);
+      myPrint(" ");
+      myPrint(quest);
+      myPrint("? ");
+      /* we will set the timedout flag to 0, since it hasn't yet */
+      timed = 0;
+
+      /* now set the interval timer prior to reading in the user's response */
+
+ /*  Problem 9 */
+
+      /* read in the user's response, this is systems programming so don't use 
+       * scanf */
+
+ /* Problem 10 */
+
+      /* test to see if the user responded and if an error has occurred 
+       * an error can actually occur as part of the functionality of this program 
+       * both SIGINT and SIGALRM will cause the read call to return a -1 
+       * this can be recognized by testing for a specific error number ...  
+       * The error is known as interrupted systems call 
+       * Should this be the case, do the following .... 
+       * If the read call was interrupted and the timedout flag isn't set 
+       * just continue 
+       * if the timedout flag was set, inform the user that time is up 
+       * then go to the next question
+       */
+      if (numRead == 0) break;
+      if (numRead == -1)
+	{
+	  if (errno == EINTR)
+	    {
+	      if (timed)
+		{
+		  myPrint("\nTime's up, next question\n");
+		  if (readQA(questFd, ansFd, quest, ans) == 0) break;
+		  question++;
+		}
+	      continue;
+	    }
+	  perror("read");
+	  exit(EXIT_FAILURE);
+	}
+      /* disable the timer here */
+/* Problem 11*/
+
+      /* we will convert the buf being read in to a c-string by tacking on a 0 */
+	buf[numRead-1] = 0;
+	/* check the answer */
+	if (strcmp(buf,ans) == 0)
+	  {
+	    correct++;
+	    myPrint("\ncorrect\n");
+	  } else
+	  {
+	    myPrint(ans);
+	    myPrint("\nwrong\n");
+	  }
+	/* read the next question .. break if there are no more questions */
+	if (readQA(questFd, ansFd, quest, ans) == 0) break;
+
+	question++;
+    }
+
+  myPrint("final score is ");
+  myPrintInt(correct);
+  myPrint(" out of ");
+  myPrintInt(question);
+
+/* Problem 12 */
+close(questFd);
+close(ansFd);
+
+exit(EXIT_SUCCESS);
+
+}

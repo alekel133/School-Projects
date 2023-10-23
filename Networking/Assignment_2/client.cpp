7e08799e8da1d6d200b61392a1698c46a7694551
Alexander Kellough <atk133@msstate.edu>
2023-10-22 19:33:10 -0500

    Networking Progress

--- Networking/Assignment_2/client.cpp
+++ Networking/Assignment_2/client.cpp
@@ -18,29 +18,95 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include "packet.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-  struct hostent *s;
+  
+  struct hostent *s; 
   s = gethostbyname(argv[1]);
-  int out_port = stoi(argv[2]);
-  int in_port = stoi(argv[3]);
+  int n_port = stoi(argv[2]);
+  int t_port = stoi(argv[3]);
   char *filename = argv[4];
 
-  struct sockaddr_in network;
+  struct sockaddr_in server;
   int mysocket = 0;
-  socklen_t slen = sizeof(network);
+  socklen_t slen = sizeof(server);
   
-  if((mysocket=socket(AF_INET, SOCK_DGRAM, 0))==-1)
-	  cout << "Error in creating socket." << endl;
+  
+  if ((mysocket=socket(AF_INET, SOCK_DGRAM, 0))==-1)
+    cout << "Error in creating socket.\n";
 
-  memset((char *) &network, 0, sizeof(network));
-  network.sin_family = AF_INET;
-  network.sin_port = htons(out_port);
-  bcopy((char *)s->h_addr,
-		  (char *)&network.sin_addr.s_addr,
-		  s->h_length);
+  
+  memset((char *) &server, 0, sizeof(server));
+  server.sin_family = AF_INET;
+  server.sin_port = htons(n_port);
+  bcopy((char *)s->h_addr, 
+	(char *)&server.sin_addr.s_addr,
+	s->h_length);
+ 
+  // obtained from https://stackoverflow.com/questions/32737083/extracting-ip-data-using-gethostbyname                                         
+  struct in_addr a;
+  printf("name: %s\n", s->h_name);
+  while (*s->h_aliases)
+      printf("alias: %s\n", *s->h_aliases++);
+  while (*s->h_addr_list)
+  {
+      bcopy(*s->h_addr_list++, (char *) &a, sizeof(a));
+      printf("address: %s\n", inet_ntoa(a));
+  }
+
+  // Declaring variables and objects for file exchange
+  int fd = open(filename, O_RDONLY);
+  int state = 0;
+  struct stat st;
+
+  // Get file size
+  fstat(fd, &st);
+  off_t size = st.st_size;
+
+  // Send filesize to server
+  if(sendto(mysocket, &size, sizeof(off_t), 0, (struct sockaddr *)&server, slen) == -1) {
+		 cout << "Error in sendto function." << endl;
+  }
+
+  // While the EOF has not been reached, continue transmitting data.
+  state = 4;
+  while(state == 4){
+
+	  // Read packet from file
+	  state = read(fd, payload, 4);
+	  if(state == -1) {
+			perror("Error in read.\n");
+			exit(EXIT_FAILURE);
+	  }
+
+	  // Send packet
+	  if(sendto(mysocket, payload, 4, 0, (struct sockaddr *)&server, slen) == -1) {
+		 cout << "Error in sendto function." << endl;
+	  }
+
+	  // Wait for acknowledgment
+	  if(recvfrom(mysocket, payload, 4, 0, (struct sockaddr *)&server, &slen) == -1) {
+			cout << "Failed to recieve." << endl;
+		}
+
+	   // If there is still data, update size and output acknowledgement.
+		if(size > 4) {
+	      cout << payload << endl;
+         size = size - 4;
+		}
+		// If EOF has been reached, only print valid characters of acknowledgment.
+		else {
+			for(int i = 0; i < size; ++i) {
+				cout << payload[i] << endl;
+			}
+			size = 0;
+		}
+  }
+
+  // Close file descriptors.
+  close(fd);
+  close(mysocket);
   return 0;
 }
--- Networking/Assignment_2/server.cpp
+++ Networking/Assignment_2/server.cpp
@@ -23,5 +23,72 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 
+	int n_port = stoi(argv[1]);
+  int t_port = stoi(argv[2]);
+  
+  struct sockaddr_in server;
+  struct sockaddr_in client;
+  int mysocket = 0;
+  int i = 0;
+  socklen_t clen = sizeof(client);
+  char payload[4];
+  
+  if ((mysocket=socket(AF_INET, SOCK_DGRAM, 0))==-1)
+    cout << "Error in socket creation.\n";
+  
+  memset((char *) &server, 0, sizeof(server));
+  server.sin_family = AF_INET;
+  server.sin_port = htons(n_port);
+  server.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (bind(mysocket, (struct sockaddr *)&server, sizeof(server)) == -1)
+    cout << "Error in binding.\n";
+
+  // Variables and objects for file transfer
+  int fd = open("upload.txt", O_CREAT | O_WRONLY | O_RDONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IXUSR);
+  off_t size;
+  char ack[4];
+
+  // Get File size from client
+  if(recvfrom(mysocket, &size, sizeof(off_t), 0, (struct sockaddr *)&client, &clen) == -1){
+		  cout << "Failed to recieve." << endl;
+  }
+
+  // While there is still data, continue transfer.
+  while(size) {
+	  // Get packet
+	  if(recvfrom(mysocket, payload, 4, 0, (struct sockaddr *)&client, &clen) == -1){
+		  cout << "Failed to recieve." << endl;
+	  }
+	
+	  // Format acknowledgement.
+	  for(int i = 0; i < 4; ++i) {
+			ack[i] = toupper(payload[i]);
+	  }
+	  
+	  // Send acknowledgement.
+	  if (sendto(mysocket, ack, 4, 0, (struct sockaddr *)&client, clen)==-1){
+        cout << "Error in sendto function.\n";
+     }
+
+	  // If there is still data left, write full packet, update size.
+     if(size > 4) {
+		  if(write(fd, payload, 4) == -1) {
+				perror("Error in write.");
+				exit(EXIT_FAILURE);
+		  }
+		  size = size -4;
+	  }
+	  // If there is no data left, write valid bytes, and set size to 0 (file empty).
+	  else {
+			for(int i = 0; i < size; ++i) {
+				write(fd, &payload[i], 1);
+			}
+			size = 0;
+	  }
+  }
+
+  // Close File Descriptors.
+  close(fd);
+  close(mysocket);
   return 0;
 }
